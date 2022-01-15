69b8760e06b6ab57e2cb3d55c9c486878cfc4995
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-01-15 11:48:38 +0530

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,12 +1,34 @@
 package com.bl.hms;
 
-import java.util.Date;
+import java.util.Objects;
 
 public class Appointment {
 
-    String patientId;
-    String doctorId;
-    Date appointmentDate;
-    int roomNumber;
+    String patientId, appointmentId, appointmentDate, doctorId;
+    long roomNumber;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Appointment that = (Appointment) o;
+        return Objects.equals(appointmentId, that.appointmentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(appointmentId);
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "patientId='" + patientId + '\'' +
+                ", appointmentId='" + appointmentId + '\'' +
+                ", appointmentDate='" + appointmentDate + '\'' +
+                ", doctorId='" + doctorId + '\'' +
+                ", roomNumber=" + roomNumber +
+                '}';
+    }
 }
+
