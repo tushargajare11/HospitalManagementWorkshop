0c4a4ddcc687d8a1eb483a5a22269c3bd0322573
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-01-15 11:49:53 +0530

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,17 +1,46 @@
 package com.bl.hms;
+
 import java.util.Map;
+import java.util.Objects;
 
 public class Doctor {
 
     enum WeekDays{
-        SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+        SUNDAY, MONDAY,TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
     }
 
-    String name;
-    String Id;
-    String specialisation;
-    long mobileNumber;
-    String emailId;
-    Map<WeekDays, String> Availability;
+    String docName;
+    String specialization;
+    String emailID;
+    String id;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return mobNo == doctor.mobNo || Objects.equals(emailID, doctor.emailID) || Objects.equals(id, doctor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 5;
+    }
+
+    long mobNo;
+    Map<WeekDays,String> availability;
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "docName='" + docName + '\n' +
+                ", specialization='" + specialization + '\n' +
+                ", emailID='" + emailID + '\n' +
+                ", id='" + id + '\n' +
+                ", mobNo=" + mobNo + '\n' +
+                ", availability=" + availability + '\n' +
+                '}';
+    }
 }
+
+
