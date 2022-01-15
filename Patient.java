7a05ef03e30d93269659a21c90d7615eff6ce244
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-01-15 11:52:35 +0530

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -1,18 +1,43 @@
 package com.bl.hms;
 
+import java.util.Objects;
+
 public class Patient {
 
     enum Gender{
-        Male, Female, Others
+        MALE, FEMALE, OTHERS
     }
 
-    String name;
-    int age;
-    long mobileNumber;
-    String Id;
-    String emailId;
-    String address;
-    String disease;
+    String name, address, emailID, disease, patientId;
+    long mobNo, age;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Patient patient = (Patient) o;
+        return mobNo == patient.mobNo || Objects.equals(emailID, patient.emailID) || Objects.equals(patientId, patient.patientId);
+    }
+
+    @Override
+    public int hashCode() {
+        return 5;
+    }
+
     Gender gender;
 
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "name='" + name + '\n' +
+                ", address='" + address + '\n' +
+                ", emailID='" + emailID + '\n' +
+                ", disease='" + disease + '\n' +
+                ", patientId='" + patientId + '\n' +
+                ", mobNo=" + mobNo + '\n' +
+                ", age=" + age + '\n' +
+                ", info=" + gender + '\n' +
+                '}';
+    }
 }
+
