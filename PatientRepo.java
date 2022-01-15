9828829eb5b83bf7f2d88922c2e20efb25e7c472
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-01-15 11:53:01 +0530

    Update PatientRepo.java

--- PatientRepo.java
+++ PatientRepo.java
@@ -1,13 +1,53 @@
 package com.bl.hms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class PatientRepo {
-    List patientList = new ArrayList();
+    public static PatientRepo instance;
+    HashSet <Patient> patientSet = new HashSet<>();
+    UserInterface userInterface = UserInterface.getInstance();
 
-    public void add(Patient patient) {
-        patientList.add(patient);
+    public static synchronized PatientRepo getInstance(){
+        if( instance == null ){
+            instance = new PatientRepo();
+        }
+        return instance;
+    }
+    void addPatient() {
+        Patient patient = userInterface.addPatient();
+        patientSet.add(patient);
+    }
 
+    Set<Patient> getPatientSet() {
+        return patientSet;
+    }
+
+    public boolean isPatientAvailable(String patientId){
+        for (Patient patient : patientSet){
+            if( patient.patientId.equals(patientId)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Patient getPatient(String id) {
+        for (Patient patient : patientSet) {
+            if (patient.patientId.equals(id)) {
+                return patient;
+            }
+        }
+        return null;
+    }
+
+    public void remove(Patient patientRemove) {
+        patientSet.remove(patientRemove);
+    }
+
+    public void printAllPatient(Set patientList) {
+        for (Patient patient : patientSet) {
+            System.out.println(patient);
+        }
     }
 }
+
