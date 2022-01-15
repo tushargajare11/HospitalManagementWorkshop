a388840eed1ca3164b714275ef952fad7e7ed3cf
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-01-15 11:51:57 +0530

    Update DoctorRepo.java

--- DoctorRepo.java
+++ DoctorRepo.java
@@ -1,13 +1,57 @@
 package com.bl.hms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DoctorRepo {
-    List doctorList = new ArrayList();
+    public static DoctorRepo instance;
+    private HashSet<Doctor> doctorSet = new HashSet<>();
 
-    public void add(Doctor doctor){
-        doctorList.add(doctor);
+    DoctorRepo() {
+    }
 
+    public static synchronized DoctorRepo getInstance() {
+        if (instance == null) {
+            instance = new DoctorRepo();
+        }
+        return instance;
+    }
+
+    void addDoctor(Doctor doctor) {
+        doctorSet.add(doctor);
+    }
+
+    Set<Doctor> getDoctorSet() {
+        return doctorSet;
+    }
+
+    public boolean isDoctorAvailable(String id) {//10
+        for (Doctor doctor : doctorSet){
+            if (doctor.id.equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Doctor getDoctor(String id) {
+        for (Doctor doctor : doctorSet) {
+            if (doctor.id.equals(id)) {
+                return doctor;
+            }
+        }
+        return null;
+    }
+
+
+    public void remove(Doctor doctor) {
+        doctorSet.remove(doctor);
+    }
+
+    public void printAllDoctor(Set doctorList) {
+        for (Doctor doctor : doctorSet) {
+            System.out.println(doctor);
+        }
     }
 }
+
