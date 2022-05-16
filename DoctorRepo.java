82ccb903bb7329647f16f2c187a174fc47b7df85
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-05-16 19:29:02 +0530

    Update DoctorRepo.java

--- DoctorRepo.java
+++ DoctorRepo.java
@@ -1,7 +1,13 @@
 package com.bl.hms;
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import static jdk.nashorn.internal.objects.NativeArray.forEach;
 
 public class DoctorRepo {
     public static DoctorRepo instance;
@@ -26,21 +32,26 @@ public class DoctorRepo {
     }
 
     public boolean isDoctorAvailable(String id) {//10
-        for (Doctor doctor : doctorSet){
-            if (doctor.id.equals(id)) {
-                return true;
-            }
-        }
-        return false;
+        //for (Doctor doctor : doctorSet){
+           // if (doctor.id.equals(id)) {
+               // return true;
+           // }
+       // }
+       // return false;
+        return doctorSet.stream().filter(doctor -> doctor.id.equals(id))
+                .collect(Collectors.toSet()).size() > 0 ? true : false;
+
     }
 
     public Doctor getDoctor(String id) {
-        for (Doctor doctor : doctorSet) {
-            if (doctor.id.equals(id)) {
-                return doctor;
-            }
-        }
-        return null;
+      //  for (Doctor doctor : doctorSet) {
+           // if (doctor.id.equals(id)) {
+            //    return doctor;
+           // }
+
+             return doctorSet.stream().filter(doctor -> doctor.id.equals(id)).findFirst().orElse(null);
+            //return doctorSet.stream().filter(doctor -> doctor.id.equals(id)).collect(Collectors.toSet()).get(0);
+        //return null;
     }
 
 
@@ -49,9 +60,10 @@ public class DoctorRepo {
     }
 
     public void printAllDoctor(Set doctorList) {
-        for (Doctor doctor : doctorSet) {
-            System.out.println(doctor);
-        }
+        //for (Doctor doctor : doctorSet) {
+           // System.out.println(doctor);
+       // }
+        doctorSet.stream().forEach(System.out :: println);
     }
 }
 
